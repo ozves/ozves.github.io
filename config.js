4b241a2133012b1a04c97c4e3114c44c49466259
ozves <ozturksev@gmail.com>
2025-11-21 13:47:32 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js DOSYASI İÇERİĞİ:
+export const firebaseConfig = {
+    apiKey: "AIzaSyCHhFAHbWnclad6yFuI-Pw9gX6F_Dx1B2c",
+    authDomain: "online-sinav-b3644.firebaseapp.com",
+    projectId: "online-sinav-b3644",
+    storageBucket: "online-sinav-b3644.firebasestorage.app",
+    messagingSenderId: "965507531268",
+    appId: "1:965507531268:web:94cfd13774a40b6cf16b51"
+};
